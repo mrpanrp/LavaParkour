97d07e8085075e5cf8820c8ac4875240afeec94b
mrpanrp <mr.panrp@gmail.com>
2017-11-05 09:15:06 -0500

    Game assets

---
+++ Assets/Motion1.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion1 : MonoBehaviour
+{
+    //public GameObject plane;
+    public static int count=0;
+    public float speed;
+    private Rigidbody rd;
+    public float fireRate;
+    private float nextFire;
+    private bool doubleJump = true;
+
+    public int getCount()
+    {
+        return count;
+    }
+    private void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        Vector3 moment = new Vector3(moveH, 0.0f, moveV);
+        rd.AddForce(moment * speed);
+        if (rd.position.y <= 0.5f)
+        {
+            rd.position = Vector3.one;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            if (doubleJump)
+            {
+                fireRate -= 1.5f;
+                doubleJump = false;
+            }
+            else
+            {
+                fireRate += 1.5f;
+                doubleJump = true;
+            }
+            nextFire = Time.time + fireRate ;
+            rd.AddForce(new Vector3(0, 300.0f, 0));
+            
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rd.velocity = Vector3.zero;
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            rd.transform.localScale += Vector3.one;
+        }
+        else if(Input.GetKey(KeyCode.J))
+        {
+            rd.transform.localScale = Vector3.one;
+        }
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            //Destroy(other.gameObject);
+            //GenerateMap();
+        }
+    }
+    
+
+}
---
+++ Assets/SoundSC.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SoundSC : MonoBehaviour
+{
+	private string[] ks;
+	private KeywordRecognizer rec;
+    private Rigidbody rd;
+    public float speed;
+
+
+    void Start()
+	{
+        rd = GetComponent<Rigidbody>();
+        ks = new string[]{"l","right","up","back","stop","pe","jump","big","small"};
+		rec = new KeywordRecognizer (ks);
+		rec.OnPhraseRecognized += recd;
+		rec.Start ();
+	}
+	private void recd(PhraseRecognizedEventArgs pre)
+	{
+		print (pre.text);
+		processCommand (pre.text);
+	}
+	void processCommand(String s)
+	{
+        if (s.Equals("l")) {
+            rd.AddForce(new Vector3(-1.0f, 0.0f, 0.0f) * speed);
+        }
+        else if (s.Equals("right")) {
+            rd.AddForce(new Vector3(1.0f, 0.0f, 0.0f) * speed);
+        }
+        else if (s.Equals("up")) {
+            rd.AddForce(new Vector3(0.0f, 0.0f, 1.0f) * speed);
+        }
+        else if (s.Equals("back")) {
+            rd.AddForce(new Vector3(0.0f, 0.0f, -1.0f) * speed);
+        }
+        else if (s.Equals("stop") || s.Equals("ting")) {
+            rd.velocity = Vector3.zero;
+        }
+        else if (s.Equals("jump")) {
+            rd.AddForce(new Vector3(0, 300.0f, 0));
+        }
+        else if (s.Equals("pe") || s.Equals("big"))
+        {
+            rd.transform.localScale += Vector3.one;
+        }
+        else if (s.Equals("small"))
+        {
+            rd.transform.localScale = Vector3.one;
+        }
+	}
+}
